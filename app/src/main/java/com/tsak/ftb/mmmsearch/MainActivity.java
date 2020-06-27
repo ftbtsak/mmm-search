bffaffc84270bf99dc1691b79ddf04539f6e79ec
ftbtsak2 <ftbtsak2@ftbtsak2.com>
2020-06-27 20:16:04 +0900

    2020/06/27 create settings layout

--- app/src/main/java/com/tsak/ftb/mmmsearch/MainActivity.java
+++ app/src/main/java/com/tsak/ftb/mmmsearch/MainActivity.java
@@ -2,13 +2,66 @@ package com.tsak.ftb.mmmsearch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.tsak.ftb.mmmsearch.settings.SettingsActivity;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static int SETTINGS_REQ_CODE = 810;
+
+    private AtomicBoolean isSearching = new AtomicBoolean(false);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu (Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+
+        menu.findItem(R.id.settings_menu).setEnabled(!isSearching.get());
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch(item.getItemId()) {
+            case R.id.settings_menu:
+                startActivityForResult(new Intent(this, SettingsActivity.class), SETTINGS_REQ_CODE);
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch(requestCode) {
+            case SETTINGS_REQ_CODE:
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/tsak/ftb/mmmsearch/settings/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.tsak.ftb.mmmsearch.settings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tsak.ftb.mmmsearch.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
